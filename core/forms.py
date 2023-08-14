a7bc57988711d34844123b87b0fe8719c6597037
wujekd <dom.wujek@gmail.com>
2023-08-14 06:42:38 +0100

    implemented a custom registration form

--- core/forms.py
+++ core/forms.py
@@ -12,16 +12,19 @@ class LoginForm(AuthenticationForm):
         'class': 'w-full py-4 px-6 rounded-xl'
     }))
     
-    
+
 
 class SignupForm(UserCreationForm):
+    cokolwiek = forms.CharField(widget=forms.TextInput, max_length=34, required=False)
+    
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'cokolwiek', 'email', 'password1', 'password2')
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder ' : 'Your username',
         'class': 'w-full py-4 px-6 rounded-xl'
     }))
+    
     email = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder ' : 'Your email',
         'class': 'w-full py-4 px-6 rounded-xl'
@@ -41,3 +44,46 @@ class SignupForm(UserCreationForm):
         if count > 0:
             raise forms.ValidationError('Ten adres email jest juz zajety!')
         return email
+
+
+class SignupForm2(forms.Form):
+    PROFESJA_CHOICES = [
+        ('Wybierz', 'Wybierz'),
+        ('Wokal/Inst', 'Wokal/Inst'),
+        ('Produkcja', 'Produkcja'),
+        ('Zadnej pracy sie nie boje', 'Zadnej pracy sie nie boje'),
+    ]
+    
+    profesja = forms.ChoiceField(
+        choices=PROFESJA_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={
+            'class': 'w-full py-4 px-6 rounded-xl',
+        })
+    )
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Username',
+        'class': 'w-full py-4 px-6 rounded-xl' }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email',
+        'class': 'w-full py-4 px-6 rounded-xl'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+        'class': 'w-full py-4 px-6 rounded-xl'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
+        'class': 'w-full py-4 px-6 rounded-xl'}))
+    
+    # def clean_email(self):
+    #     email = self.cleaned_data.get('email')
+    #     if User.objects.filter(email=email).exists():
+    #         raise forms.ValidationError('This email is already registered.')
+    #     return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 != password2:
+            raise forms.ValidationError('Passwords do not match.')
+        return password2
+    def clean_profesja(self):
+        profesja = self.cleaned_data.get('profesja')
+        if profesja == 'Wybierz':
+            raise forms.ValidationError('Wybierz z listy czym zajmujesz sie w muzyce!')
+        return profesja
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignupForm
+from .forms import SignupForm, SignupForm2
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from account.models import Account
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
 from.tokens import account_activation_token
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 # Create your views here.
@@ -37,7 +38,6 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        Account.objects.create(user=user)
         messages.success(request, 'Twoje konto zostalo pomyslnie aktywowane! Mozesz sie zalaogowac.')
         return redirect('core:login')
     else:
@@ -61,15 +61,24 @@ def activateEmail(request, user, to_email):
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = SignupForm2(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active=False
+            user = get_user_model().objects.create(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                is_active=False,
+            )
+            user.set_password(form.cleaned_data['password1'])
             user.save()
+            account = Account.objects.create(user=user)
+            account.profesja = form.cleaned_data.get('profesja')
+            account.save()
+            
             activateEmail(request, user, form.cleaned_data.get('email'))
+            
             return redirect('/')
     else:
-        form = SignupForm()
+        form = SignupForm2()
 
     return render(request, 'core/signup.html', {
         'form' : form
