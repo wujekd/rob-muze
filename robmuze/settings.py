18abfc65859bf476d0f4ab4290c1c4914e718612
Dominik Wujek <dom.wujek@gmail.com>
2023-12-05 14:15:57 +0000

    collab submission fix

--- robmuze/settings.py
+++ robmuze/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
     # 'social_django',
     'django_recaptcha',
     # 'captcha',
