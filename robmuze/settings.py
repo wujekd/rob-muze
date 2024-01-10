0cd3479fff526ecd0357f030a7c4db80e7b8ba38
Dominik Wujek <dom.wujek@gmail.com>
2024-01-10 14:57:12 +0100

    static files and media files unignored

--- robmuze/settings.py
+++ robmuze/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     # 'social_django',
-    'django_recaptcha',
+    # 'django_recaptcha',
     # 'captcha',
     'core',
     'samples',
