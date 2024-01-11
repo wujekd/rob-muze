57fdc3c70059448f7ad59921671674315e777fec
Dominik Wujek <dom.wujek@gmail.com>
2024-01-11 02:27:29 +0100

    figured translation for django forms

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV2Checkbox
+from django.utils.translation import gettext as _
 
 from account.models import Account
 
@@ -18,36 +19,36 @@ class LoginForm(AuthenticationForm):
     
 
 
-class SignupForm(UserCreationForm):
-    cokolwiek = forms.CharField(widget=forms.TextInput, max_length=34, required=False)
+# class SignupForm(UserCreationForm):
+#     cokolwiek = forms.CharField(widget=forms.TextInput, max_length=34, required=False)
     
-    class Meta:
-        model = User
-        fields = ('username', 'cokolwiek', 'email', 'password1', 'password2')
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder ' : 'Your username',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
+#     class Meta:
+#         model = User
+#         fields = ('username', 'cokolwiek', 'email', 'password1', 'password2')
+#     username = forms.CharField(widget=forms.TextInput(attrs={
+#         'placeholder ' : 'Your username',
+#         'class': 'w-full py-4 px-6 rounded-xl'
+#     }))
     
-    email = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder ' : 'Your email',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder ' : 'Your password',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder ' : 'Repeat your password',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        count = User.objects.filter(email=email).count()
-        print(count)
-        if count > 0:
-            raise forms.ValidationError('Ten adres email jest juz zajety!')
-        return email
+#     email = forms.CharField(widget=forms.TextInput(attrs={
+#         'placeholder ' : 'Your email',
+#         'class': 'w-full py-4 px-6 rounded-xl'
+#     }))
+#     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+#         'placeholder ' : 'Your password',
+#         'class': 'w-full py-4 px-6 rounded-xl'
+#     }))
+#     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+#         'placeholder ' : 'Repeat your password',
+#         'class': 'w-full py-4 px-6 rounded-xl'
+#     }))
+#     def clean_email(self):
+#         email = self.cleaned_data.get('email')
+#         count = User.objects.filter(email=email).count()
+#         print(count)
+#         if count > 0:
+#             raise forms.ValidationError('Ten adres email jest juz zajety!')
+#         return email
 
 
 class SignupForm2(forms.Form):
@@ -74,19 +75,19 @@ class SignupForm2(forms.Form):
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
         'class': 'w-full py-4 px-6 rounded-xl'}))
     
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
-    
-    # def clean_email(self):
-    #     email = self.cleaned_data.get('email')
-    #     if User.objects.filter(email=email).exists():
-    #         raise forms.ValidationError('This email is already registered.')
-    #     return email
+    # captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
+    captcha = ReCaptchaField(
+    widget=ReCaptchaV2Checkbox(),
+    error_messages={
+        'required': _('Udowodnij ze nie jestes robotem, zarejestruj sie potem.'),
+    }
+)
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
         if password1 != password2:
-            raise forms.ValidationError('Passwords do not match.')
+            raise forms.ValidationError(_('Hasla do siebie nie pasuja.'))
         return password2
     def clean_profesja(self):
         profesja = self.cleaned_data.get('profesja')
@@ -94,13 +95,13 @@ class SignupForm2(forms.Form):
             raise forms.ValidationError('Wybierz z listy czym zajmujesz sie w muzyce!')
         return profesja
     
-    # def clean_email(self):
-    #     email = self.cleaned_data.get('email')
-    #     count = User.objects.filter(email=email).count()
-    #     print(count)
-    #     if count > 0:
-    #         raise forms.ValidationError('Ten adres email jest juz zajety!')
-    #     return email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        count = User.objects.filter(email=email).count()
+        print(count)
+        if count > 0:
+            raise forms.ValidationError('Ten adres email jest juz zajety!')
+        return email
     
     
     
