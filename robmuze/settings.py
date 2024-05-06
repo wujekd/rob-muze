c99b26952571cad279ac4f868dd553a854f4834c
Dominik Wujek <dom.wujek@gmail.com>
2024-05-06 23:03:04 +0200

    added 404 handling

--- robmuze/settings.py
+++ robmuze/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@=ufwuq_glqqx9v__@kbm!_^a2jwk(p(r3u*3#fi0&$8tld$z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 LOGIN_URL ='/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
