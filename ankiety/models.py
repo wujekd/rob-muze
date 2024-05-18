a2400d4dcd82e2f180a39ccc0d907dca45d00da4
Dominik Wujek <dom.wujek@gmail.com>
2024-05-18 08:37:34 +0100

    updated polls models

--- ankiety/models.py
+++ ankiety/models.py
@@ -2,21 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class AnkietaOtw(models.Model):
-    tytol = models.CharField(max_length=40)
-    pytanie = models.CharField(max_length=500)
-    data = models.DateTimeField(auto_now_add=True)
-    kategoria = models.CharField(max_length=100)
+    title = models.CharField(max_length=40)
+    title_en = models.CharField(max_length=40, blank = True)
+    question = models.CharField(max_length=500)
+    question_en = models.TextField(max_length=500, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    tags = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.tytol
+        return self.title
 
 
 
 class OdpowiedzOtw(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ankietaotw = models.ForeignKey(AnkietaOtw, on_delete=models.CASCADE)
+    poll = models.ForeignKey(AnkietaOtw, on_delete=models.CASCADE)
     answer = models.TextField(max_length=700)
-    data = models.DateTimeField(auto_now_add=True)
+    answer_en = models.TextField(max_length=700, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.ankietaotw.tytol}"
