30e0fb82134bb4b44afce427c825df4a13ec7e9e
Dominik Wujek <dom.wujek@gmail.com>
2024-05-18 11:53:20 +0100

    polls view logic and styling

--- ankiety/models.py
+++ ankiety/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class AnkietaOtw(models.Model):
     title = models.CharField(max_length=40)
     title_en = models.CharField(max_length=40, blank = True)
-    question = models.CharField(max_length=500)
+    question = models.TextField(max_length=500)
     question_en = models.TextField(max_length=500, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    tags = models.CharField(max_length=100)
+    tags = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class AnkietaOtw(models.Model):
 
 class OdpowiedzOtw(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    poll = models.ForeignKey(AnkietaOtw, on_delete=models.CASCADE)
+    ankietaotw = models.ForeignKey(AnkietaOtw, on_delete=models.CASCADE)
     answer = models.TextField(max_length=700)
     answer_en = models.TextField(max_length=700, blank=True)
     date = models.DateTimeField(auto_now_add=True)
