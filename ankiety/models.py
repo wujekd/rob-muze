05710f9dc4266d5b1ee7895f0413f37f33c9605c
Dominik Wujek <dom.wujek@gmail.com>
2024-06-21 22:13:30 +0100

    fixed name in model method

--- ankiety/models.py
+++ ankiety/models.py
@@ -22,4 +22,4 @@ class OdpowiedzOtw(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.ankietaotw.tytol}"
+        return f"{self.user.username} - {self.ankietaotw.title}"
